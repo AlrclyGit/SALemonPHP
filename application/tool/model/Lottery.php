898eb5cc6bf9471517ceb3d7226ef4495759210e
AlrclySuzuki <alrcly@foxmail.com>
2018-12-03 12:17:14 +0800

    添加发放红包工具类

---
+++ application/tool/model/Lottery.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * name: 红包发放记录
+ * User: 萧俊介
+ * Date: 2018/12/3
+ * Time: 12:12 PM
+ * Created by LemonPHP制作委员会.
+ */
+
+namespace app\tool\model;
+
+
+class Lottery extends BaseModel
+{
+
+}

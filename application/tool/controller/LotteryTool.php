360fe545aae1313df27f6d2bae3236dc10e22bd8
AlrclySuzuki <Alrcly@foxmail.com>
2019-04-24 15:56:16 +0800

    增加抽奖控制器功能

--- application/tool/controller/LotteryTool.php
+++ application/tool/controller/LotteryTool.php
@@ -9,9 +9,93 @@
 namespace app\tool\controller;
 
 
+use app\index\model\Lottery;
+use app\index\model\Prize;
 
 class LotteryTool extends BaseTool
 {
+
+
+    /*
+     * 查询中奖记录
+     */
+    public static function getLottery()
+    {
+        $lottery = Lottery::where('open_id', session('open_id'))
+            ->field('id,open_id,create_time,update_time,delete_time',true)
+            ->find();
+        if ($lottery) {
+            return saReturn(0, 'OK', $lottery);
+        } else {
+            return saReturn(1, '你还没有中过奖呢');
+        }
+    }
+
+
+    /*
+     * 进行抽奖并入库
+     */
+    public static function setLottery()
+    {
+        // 进行抽奖
+        $prizeAll = Prize::cache(60)->select();
+        $prizeId = LotteryTool::getLotteryID($prizeAll);
+        $prize = $prizeAll[$prizeId];
+
+        // 进行奖品判断与入库
+        if ($prize['type'] == 0) { // 谢谢参与
+            $data = [
+                'open_id' => session('open_id'),
+                'prize_id' => $prizeId,
+                'prize_name' => $prize[$prizeId]['name'],
+                'prize_type' => $prize[$prizeId]['type'],
+            ];
+            Lottery::create($data);
+            $returnData = [
+                'code' => $prize['type'],
+                'name' => $prize['name'],
+            ];
+            return saReturn(0, '谢谢参与', $returnData);
+        } elseif ($prize['type'] == 1) { // 线下实物
+            Prize::where('id', $prizeId)->setDec('number');
+            $code = saRandomString();
+            $data = [
+                'open_id' => session('open_id'),
+                'prize_id' => $prizeId,
+                'prize_name' => $prize['name'],
+                'prize_type' => $prize['type'],
+                'key' => $code
+            ];
+            Lottery::create($data);
+            $returnData = [
+                'code' => $prize['type'],
+                'name' => $prize['name'],
+                'key' => $code
+            ];
+            return saReturn(0, '线下实物', $returnData);
+        } elseif ($prize['type'] == 2) { // 红包
+            Prize::where('id', $prizeId)->setDec('number');
+            $bonusT = new BonusTool();
+            $money = $bonusT->grant($prize['money']);
+            $data = [
+                'open_id' => session('open_id'),
+                'prize_id' => $prizeId,
+                'prize_name' => $prize['name'],
+                'prize_type' => $prize['type'],
+                'money' => $money
+            ];
+            Lottery::create($data);
+            $returnData = [
+                'code' => $prize['type'],
+                'name' => $prize['name'],
+                'money' => $money
+            ];
+            return saReturn(0, '红包', $returnData);
+        } else {
+            return saReturn(0, '抽奖出错了！');
+        }
+    }
+
 
     /*
      * 抽奖核心方法
